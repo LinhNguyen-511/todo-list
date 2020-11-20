440bd38a0046085f59d2c28ad38a0d93949a1887
Linh Nguyen <mlinhng05@gmail.com>
2020-11-20 14:06:05 +0100

    Add+drag+drop+delete tasks and styling

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+// ADD NEW TASK //
+// get the todo-add <input>
+var newTodo = document.getElementById('todo-add');
+newTodo.addEventListener("keyup", function(e) {
+    // if the input has text
+    // and the user press enter 
+    if(newTodo.value && e.keyCode === 13) {
+        e.preventDefault();
+        addNewTask(newTodo.value);
+        document.getElementById('todo-add').value = "";
+    }
+});
+
+
+function addNewTask(newTodo) {
+    var i = 3;
+    var list = document.getElementById("todos");
+    //create a new element
+    var newItem = document.createElement('p');
+    // change the text to what user types in
+    newItem.innerText = newTodo;
+
+    // create the div for the delete icon
+    var deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete');
+    deleteBtn.setAttribute('onclick', 'removeTask(this);');
+    var deleteIcon = document.createElement('i');
+    // add the class attr of icon
+    deleteIcon.className = 'far fa-trash-alt';
+    deleteBtn.appendChild(deleteIcon);
+
+    //  create the outside div
+    var itemDiv = document.createElement('div');
+    itemDiv.classList.add('draggable');
+    itemDiv.id = 'draggable-' + ++i;
+    itemDiv.setAttribute('draggable','true');
+    itemDiv.setAttribute('ondragstart','dragStart_handler(event);');
+    itemDiv.setAttribute('ondragend','dragend_handler(event);');
+    itemDiv.appendChild(newItem);
+    itemDiv.appendChild(deleteBtn);
+    // insert new todo before the first todo  
+    // and after the header
+	list.appendChild(itemDiv);
+}
+
+function removeTask(taskId) {
+    var removedTask = taskId.parentNode;
+    removedTask.parentNode.removeChild(removedTask);
+}
+
+// DRAG N DROP //
 // 1. Identify what is draggable
 function dragStart_handler(ev) {
     console.log("dragStart");
